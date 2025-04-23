7fb801f434886cd899d7075082094343e5ce9cf0
aokoye <aokoye@gmail.com>
2025-04-23 01:26:17 -0700

    Updated home page.

--- src/home.js
+++ src/home.js
@@ -1,6 +1,7 @@
 const data = {
     title: 'CoffeeCha', 
-    body: 'CoffeeCha is a coffee and bubble tea shop. We buy our coffee beans from local roasters who work carefully with the farms they buy their raw beans from. We import our tea directly from China and Japan and make are boba by scratch in house multiple times a day.'
+    body: 'CoffeeCha is a coffee and bubble tea shop. We buy our coffee beans from local roasters who work carefully with the farms they buy their raw beans from. We import our tea directly from China and Japan and make are boba by scratch in house multiple times a day.',
+    custom: 'We believe strongly in being able to cater to many food needs. To this end we do not charge extra for non-dairy and lactose free milks. Additionally we use separate cups and utensils for drinks that are dairy free. Additionally all of our bubble tea toppings are vegan and soy free.'
 }
 
 export const renderPage = () => {
